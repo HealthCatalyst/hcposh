a5987235057061dd4eb8234c80b959cc1d6ea0cb
Nathan Given <nathan.given@healthcatalyst.com>
2023-05-02 20:41:23 -0600

    adding more debug

--- HCPosh.psm1
+++ HCPosh.psm1
@@ -201,6 +201,7 @@ function HCPosh {
                 $OutDataArray = HCPosh @params | Where-Object { $_ };
                 forEach ($OutData in $OutDataArray) {
                     write-host "Outdata Type: $($OutData.gettype())"
+                    write-host "OutData.Entities type: $($OutData.Entities.gettype())"
                     $NewOutDir = $OutDir + '\' + $OutData._hcposh.FileBaseName
                     if ($OutZip) {
                         if ($OutVar) {
